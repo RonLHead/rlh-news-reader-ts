4ff91255f2601f64caf1d06a3567e9ddbe4d713f
Ronald Head <ronaldlhead@gmail.com>
2022-07-27 17:29:13 -0600

    Map sections from the stories fetch into form element

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,20 @@ interface HeaderProps {
 }
 
 const Header: FC<HeaderProps> = ({ stories }) => {
-  
+  let allSections = stories.flatMap(story => story.section).reduce((storySections:string[], section) => {
+    if(!storySections.includes(section)) {
+      storySections.push(section);
+    }
+    return storySections;
+  }, []);
+
+  let sectionList = allSections.sort().map((section:string) => {
+    return (
+      <>
+        <option value={section} key={section}>{section}</option>
+      </>
+    )
+  })
   return (
     <header className='Header'>
       <h1 className='Header-title'>NYT News Reader</h1>
@@ -15,7 +28,8 @@ const Header: FC<HeaderProps> = ({ stories }) => {
         <h2 className='home'>Home</h2>
         <form>
           <select className='section-select'>
-            <option>Filter by Category:</option>
+            <option value="" disabled selected hidden key='default'>Filter by Category:</option>
+            {sectionList}
           </select>
         </form>
         <input type='text' placeholder='Search' className='search' />
