648ed96c7425fbf550e8d4010337ca73f44188c7
Ronald Head <ronaldlhead@gmail.com>
2022-07-27 09:57:42 -0600

    Remove title from App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,7 +9,6 @@ function App() {
   return (
     <main className="App">
       <Header />
-      <h1>News Reader</h1>
       <h2>Top story component goes here</h2>
       <h3>Stories go here</h3>
       <p>Story</p>
