c1ebd17eb3617fe30bcf11fff3e024a192e0e3f3
Ronald Head <ronaldlhead@gmail.com>
2022-07-28 17:32:46 -0600

    Refactor Header: remove dynamic section list, hardcode sections for filter

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,13 @@
 import { FC } from 'react';
 import { isPropertySignature } from 'typescript';
-import { IStories } from '../App/App';
 import './Header.css';
 
 interface HeaderProps {
-  stories:IStories[];
   getSection: (section:string) => void;
 }
 
-const Header: FC<HeaderProps> = ({ stories, getSection }) => {
-  let allSections = stories.flatMap(story => story.section).reduce((storySections:string[], section) => {
-    if(!storySections.includes(section)) {
-      storySections.push(section);
-    }
-    return storySections;
-  }, []);
+const Header: FC<HeaderProps> = ({ getSection }) => {
 
-  let sectionList = allSections.sort().map((section:string) => {
-    return (
-      <>
-        <option value={section} key={section}>{section}</option>
-      </>
-    )
-  })
   return (
     <header className='Header'>
       <h1 className='Header-title'>NYT News Reader</h1>
@@ -31,8 +16,30 @@ const Header: FC<HeaderProps> = ({ stories, getSection }) => {
         <form>
           <select className='section-select' onChange={e => getSection(e.target.value)}>
             <option value="" disabled selected hidden key='default'>Filter by Category:</option>
-            <option value='home' key='all'>all</option>
-            {sectionList}
+            <option value='home' key='all'>All</option>
+            <option value='arts'>Arts</option>
+            <option value='automobiles'>Automobiles</option>
+            <option value='books'>Books</option>
+            <option value='business'>Business</option>
+            <option value='fashion'>Fashion</option>
+            <option value='food'>Food</option>
+            <option value='health'>Health</option>
+            <option value='insider'>Insider</option>
+            <option value='magazine'>Magazine</option>
+            <option value='nyregion'>NY Region</option>
+            <option value='obituaries'>Obituaries</option>
+            <option value='opinion'>Opinion</option>
+            <option value='politics'>Politics</option>
+            <option value='realestate'>Real Estate</option>
+            <option value='Science'>Science</option>
+            <option value='Sports'>Sports</option>
+            <option value='sundayreview'>Sunday Review</option>
+            <option value='technology'>Technology</option>
+            <option value='theater'>Theater</option>
+            <option value='t-magazine'>T-Magazine</option>
+            <option value='upshot'>Upshot</option>
+            <option value='us'>U.S.</option>
+            <option value='world'>World</option>
           </select>
         </form>
         <input type='text' placeholder='Search' className='search' />
