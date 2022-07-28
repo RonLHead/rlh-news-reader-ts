ce98f1b72d47794221bfdce886add04809655df5
Ronald Head <ronaldlhead@gmail.com>
2022-07-28 09:03:52 -0600

    Set getSection function prop on click to filter form element

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
+import { isPropertySignature } from 'typescript';
 import { IStories } from '../App/App';
 import './Header.css';
 
 interface HeaderProps {
   stories:IStories[];
+  getSection: (section:string) => void;
 }
 
-const Header: FC<HeaderProps> = ({ stories }) => {
+const Header: FC<HeaderProps> = ({ stories, getSection }) => {
   let allSections = stories.flatMap(story => story.section).reduce((storySections:string[], section) => {
     if(!storySections.includes(section)) {
       storySections.push(section);
@@ -27,8 +29,9 @@ const Header: FC<HeaderProps> = ({ stories }) => {
       <nav className='Nav'>
         <h2 className='home'>Home</h2>
         <form>
-          <select className='section-select'>
+          <select className='section-select' onChange={e => getSection(e.target.value)}>
             <option value="" disabled selected hidden key='default'>Filter by Category:</option>
+            <option value='home' key='all'>all</option>
             {sectionList}
           </select>
         </form>
