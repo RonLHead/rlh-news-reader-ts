8aa27cd4558fcd910711f07f770de122d5245da3
Ronald Head <ronaldlhead@gmail.com>
2022-07-29 11:19:07 -0600

    Set up SingleStory display using stories passed in as props and accessed through the id in the Route

--- src/components/SingleStory/SingleStory.tsx
+++ src/components/SingleStory/SingleStory.tsx
@@ -1,8 +1,47 @@
+import { FC } from 'react';
+import { useParams } from 'react-router-dom';
+import { IStories } from '../App/App';
 import './SingleStory.css';
 
-const SingleStory = () => {
+interface HeaderProps {
+  stories:IStories[];
+}
+
+const SingleStory: FC<HeaderProps>= ({ stories }) => {
+  const { id } = useParams();
+  const story = stories[Number(id)];
+  let pubDate;
+  let pubYear;
+  let pubMoDay;
+
+  let storyDetails;
+  
+  if(!story) {
+    storyDetails = <h1>Artile is loading...</h1>
+  } else {
+    pubDate = story.published_date.split('').reverse().splice(15, 15).reverse();
+    pubYear = pubDate.slice(0, 4).join('');
+    pubMoDay = pubDate.slice(5, 10).join('');
+
+    storyDetails = (
+      <div>
+        <h1>{story.title}</h1>
+        <h2>{story.byline}</h2>
+        <p>{pubMoDay}-{pubYear}</p>
+        <a href={story.url} target="_blank" rel="noopener noreferrer">
+          <img className='SingleStory-img' alt={story.multimedia[0].caption} src={story.multimedia[0].url}/>
+        </a>
+        <p>{story.multimedia[0].caption}</p>
+        <p>{story.abstract}</p>
+      </div>
+    )
+  }
+
+
   return (
-    <p>SingleStory component</p>
+    <div>
+      {storyDetails}
+    </div>
   )
 }
 
