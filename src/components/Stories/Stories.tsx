525458e457fdfca33374f9e85607744a43088458
Ronald Head <ronaldlhead@gmail.com>
2022-07-29 11:19:43 -0600

    Add Link tag to Stories component

--- src/components/Stories/Stories.tsx
+++ src/components/Stories/Stories.tsx
@@ -1,5 +1,6 @@
 import ReactLoading from 'react-loading';
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import { IStories } from '../App/App';
 import './Stories.css';
 
@@ -11,22 +12,24 @@ const Stories: FC<HeaderProps> = ({ stories }) => {
 
   let storiesList = stories.map(story => {
     return (
-      <section key={stories.indexOf(story)}>
-        <div className='storiesList-container'>
-          <div className='storiesList-info-container'>
-            <h3 className='story-title'>{story.title}</h3>
-            <p className='story-abstract'>{story.abstract}</p>
-            <p>{story.byline}</p>
+      <section key={stories.indexOf(story)} id={stories.indexOf(story).toString()}>
+        <Link to={`${story.section}/${stories.indexOf(story).toString()}`} style={{ textDecoration: 'none', color: 'black' }}>
+          <div className='storiesList-container'>
+            <div className='storiesList-info-container'>
+              <h3 className='story-title'>{story.title}</h3>
+              <p className='story-abstract'>{story.abstract}</p>
+              <p>{story.byline}</p>
+            </div>
+            {!story.multimedia ? (
+              <ReactLoading 
+              type='bubbles'
+              color='gray'
+              width={'10%'}
+              height={'10%'}
+              />
+              ) : <img className='story-img' alt={story.multimedia[2].caption} src={story.multimedia[2].url}/>}
           </div>
-          {!story.multimedia ? (
-            <ReactLoading 
-            type='bubbles'
-            color='gray'
-            width={'10%'}
-            height={'10%'}
-            />
-            ) : <img className='story-img' alt={story.multimedia[2].caption} src={story.multimedia[2].url}/>}
-        </div>
+        </Link>
         <hr 
           style={{
             backgroundColor: '#F1F2F2',
