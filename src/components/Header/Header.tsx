115a87236aa47e567889d11face90900bc577636
Ronald Head <ronaldlhead@gmail.com>
2022-08-02 15:38:04 -0600

    Remove Search bar and refactor Home button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
-import { NavLink, } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import './Header.css';
 
 interface HeaderProps {
@@ -8,6 +7,8 @@ interface HeaderProps {
 }
 
 const Header: FC<HeaderProps> = ({ getSection }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
 
   return (
     <header className='Header'>
@@ -15,39 +16,46 @@ const Header: FC<HeaderProps> = ({ getSection }) => {
         <button className='Header-title' onClick={e => getSection('home')}>NYT News Reader</button>
       </NavLink>
       <nav className='Nav'>
-        <NavLink to='/' style={{ textDecoration: 'none', color: 'white' }}>
-          <button className='home'>Home</button>
-        </NavLink>
-        <form>
-          <select id='category-select' className='section-select' onChange={e => getSection(e.target.value)}>
-            <option value="" disabled selected hidden key='default'>Filter by Category:</option>  
-            <option value='home' key='all'>All</option>
-            <option value='arts'>Arts</option>
-            <option value='automobiles'>Automobiles</option>
-            <option value='books'>Books</option>
-            <option value='business'>Business</option>
-            <option value='fashion'>Fashion</option>
-            <option value='food'>Food</option>
-            <option value='health'>Health</option>
-            <option value='insider'>Insider</option>
-            <option value='magazine'>Magazine</option>
-            <option value='nyregion'>NY Region</option>
-            <option value='obituaries'>Obituaries</option>
-            <option value='opinion'>Opinion</option>
-            <option value='politics'>Politics</option>
-            <option value='realestate'>Real Estate</option>
-            <option value='Science'>Science</option>
-            <option value='Sports'>Sports</option>
-            <option value='sundayreview'>Sunday Review</option>
-            <option value='technology'>Technology</option>
-            <option value='theater'>Theater</option>
-            <option value='t-magazine'>T-Magazine</option>
-            <option value='upshot'>Upshot</option>
-            <option value='us'>U.S.</option>
-            <option value='world'>World</option>
-          </select>
-        </form>
-        <input type='text' placeholder='Search' className='search' />
+        {location.pathname === '/' ? (
+          <div className='Nav-wrapper'>
+            <NavLink to='/' style={{ textDecoration: 'none', color: 'white' }}>
+              <button className='home' onClick={e => getSection('home')}>Home</button>
+            </NavLink>
+            <form>
+              <select id='category-select' className='section-select' onChange={e => getSection(e.target.value)}>
+                <option value="" disabled selected hidden key='default'>Filter by Category:</option>  
+                <option value='home' key='all'>All</option>
+                <option value='arts'>Arts</option>
+                <option value='automobiles'>Automobiles</option>
+                <option value='books'>Books</option>
+                <option value='business'>Business</option>
+                <option value='fashion'>Fashion</option>
+                <option value='food'>Food</option>
+                <option value='health'>Health</option>
+                <option value='insider'>Insider</option>
+                <option value='magazine'>Magazine</option>
+                <option value='nyregion'>NY Region</option>
+                <option value='obituaries'>Obituaries</option>
+                <option value='opinion'>Opinion</option>
+                <option value='politics'>Politics</option>
+                <option value='realestate'>Real Estate</option>
+                <option value='Science'>Science</option>
+                <option value='Sports'>Sports</option>
+                <option value='sundayreview'>Sunday Review</option>
+                <option value='technology'>Technology</option>
+                <option value='theater'>Theater</option>
+                <option value='t-magazine'>T-Magazine</option>
+                <option value='upshot'>Upshot</option>
+                <option value='us'>U.S.</option>
+                <option value='world'>World</option>
+              </select>
+            </form>
+          </div>
+        ) : (
+          <div className='Nav-go-back'>
+            <button onClick={() => navigate(-1)} className='home'>Go Back</button>
+          </div>
+        )}
       </nav>
     </header>
   )
