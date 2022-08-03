408a3c9d6bfc53b474d60ee882529bf2ca6f6831
Ronald Head <ronaldlhead@gmail.com>
2022-08-03 17:03:04 -0600

    Import css for Error and error message state

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,6 +1,12 @@
-const Error = () => {
+import { FC } from 'react';
+import './Error.css';
+
+interface HeaderProps {
+  error:string;
+}
+const Error: FC<HeaderProps> = ({ error }) => {
   return (
-    <p>Unable to load. Please try again later.</p>
+    <p className='error-text'>{error}</p>
   )
 }
 
