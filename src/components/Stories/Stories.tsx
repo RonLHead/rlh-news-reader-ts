489c5eb1dc8863a56f5918072ab60c3a6a5f8ad5
Ronald Head <ronaldlhead@gmail.com>
2022-08-03 16:31:23 -0600

    Import error and isLoading as state into Stories

--- src/components/Stories/Stories.tsx
+++ src/components/Stories/Stories.tsx
@@ -8,42 +8,50 @@ import './Stories.css';
 interface HeaderProps {
   stories:IStories[];
   topStory:IStories;
+  isLoading:boolean;
+  error:string;
 }
 
-const Stories: FC<HeaderProps> = ({ stories, topStory }) => {
+const Stories: FC<HeaderProps> = ({ stories, topStory, isLoading, error }) => {
+  let storiesList;
 
-  let storiesList = stories.map(story => {
-    return (
-      <section key={stories.indexOf(story)} id={stories.indexOf(story).toString()}>
-        <Link to={`${story.section}/${stories.indexOf(story).toString()}`} style={{ textDecoration: 'none', color: 'black' }}>
-          <div className='storiesList-container'>
-            <div className='storiesList-info-container'>
-              <h3 className='story-title'>{story.title}</h3>
-              <p className='story-abstract'>{story.abstract}</p>
-              <p>{story.byline}</p>
+  if(!stories.length) {
+    storiesList = <h3>{error}</h3>
+  } else {
+    storiesList = stories.map(story => {
+      return (
+        <section key={stories.indexOf(story)} id={stories.indexOf(story).toString()}>
+          <Link to={`${story.section}/${stories.indexOf(story).toString()}`} style={{ textDecoration: 'none', color: 'black' }}>
+            <div className='storiesList-container'>
+              <div className='storiesList-info-container'>
+                <h3 className='story-title'>{story.title}</h3>
+                <p className='story-abstract'>{story.abstract}</p>
+                <p>{story.byline}</p>
+              </div>
+              {!story.multimedia ? (
+                <ReactLoading 
+                  type='bubbles'
+                  color='gray'
+                  width={'10%'}
+                  height={'10%'}
+                />
+                ) : <img className='story-img' alt={story.multimedia[2].caption} src={story.multimedia[2].url}/>}
             </div>
-            {!story.multimedia ? (
-              <ReactLoading 
-                type='bubbles'
-                color='gray'
-                width={'10%'}
-                height={'10%'}
-              />
-              ) : <img className='story-img' alt={story.multimedia[2].caption} src={story.multimedia[2].url}/>}
-          </div>
-        </Link>
-        <hr 
-          style={{
-            backgroundColor: '#F1F2F2',
-            border: 'none',
-            borderRadius: '5px',
-            height: 5,
-            width: '88vw',
-          }}
-        />
-      </section>
-    )
-  })
+          </Link>
+          <hr 
+            style={{
+              backgroundColor: '#F1F2F2',
+              border: 'none',
+              borderRadius: '5px',
+              height: 5,
+              width: '88vw',
+            }}
+          />
+        </section>
+      )
+    })
+  }
+  
   return (
     <section className='stories-wrapper'>
       <div className='story-container'>
